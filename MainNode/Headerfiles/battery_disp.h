e4a9cf6f3065eaa4754dd62b2b2e44003a035d27
saimadhuri-1310 <saimadhurisamineedi@gmail.com>
2026-09-01 22:10:36 +0530

    Add files via upload

---
+++ MainNode/Headerfiles/battery_disp.h
@@ -0,0 +1,9 @@
+#ifndef _BATTERY_DISP_H_
+#define _BATTERY_DISP_H_
+void StoreCustCharFont(unsigned char p);
+void StoreCustCharFont1(unsigned char p);
+void StoreCustCharFont2(unsigned char p);
+void StoreCustCharFont3(unsigned char p);
+void StoreCustCharFont4(unsigned char p);
+#endif
+
